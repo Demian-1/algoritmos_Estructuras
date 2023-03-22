9d9dea84a88b9a9680bc970ebfb7b4a8c9ae3175
Demian-1 <sergio_demian_ae@hotmail.com>
2023-03-21 20:29:06 -0600

    terminando la queque

--- queue.c
+++ queue.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #define SIZE 5
-int rear, front;
-int items[SIZE];
+
+int rear=-1, front=-1, values[SIZE]; 
+
 void enQueue(int value){
     if (rear ==  SIZE - 1){
         printf("Nuestro queue esta lleno \n");
@@ -10,11 +11,32 @@ void enQueue(int value){
             front = 0;
         }
         rear++;
-        items[rear] = value;
+        values[rear] = value;
 
         printf("Se inserto el valor %d, en la poscicion %d correctamente \n", value, rear);
     }
-
-
 }
 
+void deQueue(){
+    if(front == -1){
+        printf("No hay nada para sacar \n");
+    } else{
+        printf("Se elimino el valor %d", values[front]);
+        front++;
+        rear++;
+        if(front > rear){   // 5 > 4
+            front = rear = -1;
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    enQueue(10);
+    enQueue(1);
+    enQueue(12);
+    enQueue(13);
+    enQueue(14);
+    enQueue(14);
+    return 0;
+}
